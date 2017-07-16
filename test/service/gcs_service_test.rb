8ddcc3df67e385439dc49b263ee9726fd241f555
Michael Herold <michael.j.herold@gmail.com>
2017-07-16 12:23:55 -0500

    Add direct upload support to GCS service
    Google Cloud Storage does not ask you to (or allow?) specify the content
    length up front, so that parameter is ignored right now. Otherwise, the
    functionality is similar to the S3 service's direct upload capability.
    
    The integration test for uploading the written URL was failing similarly
    to David's S3 test when using HTTParty. Dropping down to use Net::HTTP
    allows the test to complete successfully by writing and then downloading
    the resulting file.

--- test/service/gcs_service_test.rb
+++ test/service/gcs_service_test.rb
@@ -1,10 +1,26 @@
 require "service/shared_service_tests"
+require "net/http"
+require "uri"
 
 if SERVICE_CONFIGURATIONS[:gcs]
   class ActiveStorage::Service::GCSServiceTest < ActiveSupport::TestCase
     SERVICE = ActiveStorage::Service.configure(:gcs, SERVICE_CONFIGURATIONS)
 
     include ActiveStorage::Service::SharedServiceTests
+
+    test "direct upload" do
+      begin
+        key = SecureRandom.base58(24)
+        data = "Something else entirely!"
+        direct_upload_url = @service.url_for_direct_upload(key, expires_in: 5.minutes, content_type: "text/plain", content_length: data.size)
+
+        write_to_url(data, direct_upload_url)
+
+        assert_equal data, @service.download(key)
+      ensure
+        @service.delete key
+      end
+    end
 
     test "signed URL generation" do
       travel_to Time.now do
@@ -13,6 +29,23 @@ if SERVICE_CONFIGURATIONS[:gcs]
 
         assert_equal url, @service.url(FIXTURE_KEY, expires_in: 2.minutes, disposition: :inline, filename: "test.txt")
       end
+    end
+
+    def write_to_url(data, url)
+      url = URI(url)
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Put.new(url)
+      request["content-type"] = "text/plain"
+      request["origin"] = "http://localhost:3000"
+      request["cache-control"] = "no-cache"
+      request.body = data
+
+      response = http.request(request)
+      puts response.read_body
     end
   end
 else
