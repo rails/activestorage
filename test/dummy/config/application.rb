d178c8ec70d56b6c8d26fade892163f8ec4ece30
dixpac <dino.onex@gmail.com>
2017-07-26 10:08:28 +0200

    Remove ActionMailer

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,13 +1,15 @@
 require_relative 'boot'
 
+require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
+#require "action_mailer/railtie"
+#require "rails/test_unit/railtie"
+#require "action_cable/engine"
 
 Bundler.require(*Rails.groups)
 require "active_storage"
