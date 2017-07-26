415395ab5ddc72b7b5292552d3037df1a4c204e9
dixpac <dino.onex@gmail.com>
2017-07-26 09:56:32 +0200

    Remove ActionCable

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,13 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 require "active_storage"
