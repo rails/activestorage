b6ee9515227d01eae9823b6bfb78ae14a9ca0de1
George Claghorn <george.claghorn@gmail.com>
2017-08-04 19:28:38 -0400

    Remove source code following merge into Rails

--- lib/active_storage/version.rb
+++
@@ -1,8 +0,0 @@
-require_relative "gem_version"
-
-module ActiveStorage
-  # Returns the version of the currently loaded ActiveStorage as a <tt>Gem::Version</tt>
-  def self.version
-    gem_version
-  end
-end
--- test/database/create_users_migration.rb
+++
@@ -1,7 +0,0 @@
-class ActiveStorageCreateUsers < ActiveRecord::Migration[5.1]
-  def change
-    create_table :users do |t|
-      t.string :name
-    end
-  end
-end
